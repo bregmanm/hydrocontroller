633c7dc9e552a9024253b5cb59759fa91ae965e2
Mikhail Bregman <bregmanm@mail.ru>
2018-03-29 23:32:32 +0300

    Add print status

--- hydrocontroller.ino
+++ hydrocontroller.ino
@@ -17,6 +17,9 @@ int incomingByte = 0;	// for incoming serial data
 void printHelp() {
 	Serial.println("Available commands:");
 	Serial.println("H or h - print help");
+	Serial.println("A or a - switch to the automatic mode");
+	Serial.println("M or m - switch to the manual mode");
+	Serial.println("S or s - print current status");
 }
 
 void setup_automatic() {
@@ -27,6 +30,11 @@ void setup_automatic() {
 void setup_manual() {
 	mode = manual;
 	Serial.println("Manual mode");
+}
+
+void print_status() {
+	Serial.print("Current mode is ");
+	Serial.println(mode == automatic?"automatic":"manual");
 }
 
 // the setup routine runs once when you press reset:
@@ -42,18 +50,21 @@ void loop() {
 		incomingByte = Serial.read();
 		if (isAlpha(incomingByte)) {
 			Serial.println(char(incomingByte));
+			if (incomingByte >= 'a') {
+				incomingByte -= 'a' - 'A';
+			}
 			switch (incomingByte) {
 				case 'H':
-				case 'h':
 					printHelp();
 					break;
-				case 'a':
 				case 'A': // Switch to automatic mode
 					setup_automatic();
 					break;
-				case 'm':
 				case 'M': // Switch to manual mode
 					setup_manual();
+					break;
+				case 'S': // get status
+					print_status();
 					break;
 				default:
 					Serial.println("Wrong command");
