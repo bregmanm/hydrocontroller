4129f7d2ce316c4669a508ce4716bb947764f60a
Mikhail Bregman <bregmanm@mail.ru>
2018-03-29 23:00:59 +0300

    First version of serial monitor: help only is available

---
+++ hydrocontroller.ino
@@ -0,0 +1,47 @@
+/*
+  Hydrocontroller implementation and remote serial monitor.
+
+*/
+
+#define BAUD_RATE 9600
+
+int incomingByte = 0;	// for incoming serial data
+
+void printHelp() {
+	Serial.println("Available commands:");
+	Serial.println("H or h - print help");
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+	Serial.begin(BAUD_RATE);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+	if (Serial.available() > 0) {
+		// read the incoming byte:
+		incomingByte = Serial.read();
+		if (isAlpha(incomingByte)) {
+			Serial.println(incomingByte);
+			switch (incomingByte) {
+				case 'H':
+				case 'h':
+					printHelp();
+					break;
+				default:
+					Serial.println("Wrong command");
+					printHelp();
+			}
+		}
+	}
+			
+	/*	
+
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1);        // delay in between reads for stability
+  */
+}
