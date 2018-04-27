62c5b43b152fcd6a0a1658a7c33bcf41df216b12
mbregman <mbregman@griddynamics.com>
2018-04-27 09:14:30 +0300

    tmp

--- analog_comp_test.ino
+++ analog_comp_test.ino
@@ -2,16 +2,32 @@
 
 int analogWritePin = 3;      // output pin which generates analog voltage
 int currOutputValue = 0;
+volatile boolean triggered;
+
+ISR (ANALOG_COMP_vect)
+  {
+  triggered = true;
+  }
 
 void setup()
 {
   Serial.begin(BAUD_RATE);
   pinMode(analogWritePin, OUTPUT);   // sets the pin as output
   analogWrite(analogWritePin, currOutputValue);
+  ADCSRB = 0;           // (Disable) ACME: Analog Comparator Multiplexer Enable
+  ACSR =  bit (ACI)     // (Clear) Analog Comparator Interrupt Flag
+        | bit (ACIE)    // Analog Comparator Interrupt Enable
+        | bit (ACIS1);  // ACIS1, ACIS0: Analog Comparator Interrupt Mode Select (trigger on falling edge)
+
 }
 
 void loop()
 {
+  if (triggered)
+    {
+    Serial.println ("Triggered!"); 
+    triggered = false;
+    }
 }
 
 void serialEvent() {
